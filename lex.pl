62cd74e09f79b86a711fd86930a0c650aceaf64a
Jakob Joachim <jak.joachim@gmail.com>
2016-10-20 14:49:24 +0200

    lex done

--- lex.pl
+++ lex.pl
@@ -1,4 +1,14 @@
 % Autor:
 % Datum: 20.10.2016
 
-lex(wer,
+% lex(wort,semantik,wortart,numerus)
+lex(wer,_,ip,_).
+lex(ist,_,ist,s).
+lex(sind,_,ip,p).
+lex(der,_,art,s).
+lex(von,_,prep,_).
+
+lex(vater,ist_vater_von,n,s).
+
+lex(Name,Name,name,s).
+
