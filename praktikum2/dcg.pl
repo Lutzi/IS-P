9424c380fa089dd6546236eba6e7cdbf832d3a17
Jakob Joachim <jak.joachim@gmail.com>
2016-10-31 12:00:00 +0100

    laeuft

--- praktikum2/dcg.pl
+++ praktikum2/dcg.pl
@@ -1,13 +1,40 @@
 % Autor:
 % Datum: 20.10.2016
 
-% :- ensure_loaded([dictionary]).
 :- consult('readsentence').
 :- consult('stammbaum').
+:- consult('lex').
 
 
 ask :-
-    writeln('Ask a question'),
-    read_sentence(In),
-    % trim(Q,In),
-    processInput(In).
+    writeln('stell ne frage huso'),
+    read_sentence(Frage),
+    trim(Frag,Frage),
+    verarbeiten(Frag, Sem).
+    
+    trim(Q,In) :- append(Q,['.'],In).
+
+verarbeiten --> entfrag.
+%High Level Shit
+entfrag --> ipe, en(Sem), np, prep, en(Sem), {call(Sem)}.
+%ergfrag -->.
+
+  %Basic Shit
+np --> art(N), nom(N, Sem).
+%np --> art(N), nom(N, Sem), ppp.
+%np --> en.
+
+ppp --> pp, np.
+
+vp --> v.
+vp --> v, np.
+
+%Lexikonzugriff
+art(N)      --> [X], {lex(X,_,art,N)}.
+nom(N, Sem) --> [X], {lex(X,Sem,n,N)}.
+ut          --> [X], {lex(X,_,ut,_)}.
+prep        --> [X], {lex(X,_,prep,_)}.
+verb        --> [X], {lex(X,_,v,_)}.
+ip        --> [X], {lex(X,_,ip,_)}.
+ipe        --> [X], {lex(X,_,ipe,_)}.
+en(Sem)        --> [X], {lex(X,Sem,name,_)}.
--- praktikum2/lex.pl
+++ praktikum2/lex.pl
@@ -3,12 +3,32 @@
 
 % lex(wort,semantik,wortart,numerus)
 lex(wer,_,ip,_).
-lex(ist,_,ist,s).
-lex(sind,_,ip,p).
+lex(ist,_,ipe,s).
+lex(sind,_,ipe,p).
+
+lex(ist,_,v,s).
+
+lex(schwester, ist_schwester_von, n, s).
+lex(bruder, ist_bruder_von, n, s).
+lex(onkel, ist_onkel_von, n, s).
+lex(tante, ist_tante_von, n, s).
+lex(vater, ist_vater_von, n, s).
+lex(mutter, ist_mutter_von, n, s).
+lex(sohn, ist_sohn_von, n, s).
+lex(tochter, ist_tochter_von, n, s).
+lex(neffe, ist_neffe_von, n, s).
+lex(halbschwester, ist_halbschwester_von, n, s).
+lex(kinder, ist_kinder_von, n, p).
+
 lex(der,_,art,s).
+lex(die,_,art,s).
+lex(die,_,art,p).
+lex(das,_,art,s).
+
 lex(von,_,prep,_).
 
-lex(vater,ist_vater_von,n,s).
+lex(ein, _, ut, s).
+lex(eine, _, ut, s).
 
-lex(Name,Name,name,s).
+lex(Name,Name,name,s):-maennlich(Name);weiblich(Name).
 
